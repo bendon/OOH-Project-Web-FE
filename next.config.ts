a1b723b7a10d4516e8fb717dd7a3f6121f8f52a9
dunstana <dunstan@afyarekod.com>
2025-02-18 16:27:19 +0300

    updated gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   experimental: {
     middlewarePrefetch: "strict", // Ensure middleware runs correctly
   },
-  output: 'standalone',
   /* config options here */
 };
 
