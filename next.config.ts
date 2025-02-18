0eeed9d25776f5cd422e4c39e5a834a5bba34a53
dunstana <dunstan@afyarekod.com>
2025-02-18 16:20:14 +0300

    updated gitignore

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     middlewarePrefetch: "strict", // Ensure middleware runs correctly
-    turbopack: false, // Disable Turbopack (use Webpack instead)
   },
   output: 'standalone',
   /* config options here */
