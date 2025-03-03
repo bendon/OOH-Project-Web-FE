b86e3d9ba017566cdd2aa5e6273c0b097536696b
dunstana <dunstan@afyarekod.com>
2025-03-03 16:09:09 +0300

    update

--- src/pages/Billboards/ManageBillBoardPage.jsx
+++ src/pages/Billboards/ManageBillBoardPage.jsx
@@ -7,18 +7,24 @@ import { ShimmerTable, ShimmerTableRow } from 'react-shimmer-effects'
 export default function ManageBillBoardPage() {
   const [billboards, setBillboards] = useState(null)
   const [loadingTable, setLoadingTable] = useState(true)
-   const [billboardReport, setBillboardReport] = useState(null)
-  
-    useEffect(() => {
-  
-      const fetchBillBoardReport = async () => {
-        const res = await getBoardReport()
-        if (res.status === 200) {
-          setBillboardReport(res.data)
-        }
+  const [billboardReport, setBillboardReport] = useState(null)
+  const [paging, setPaging] = useState({
+    page: 1,
+    size: 10,
+    total_pages: 0,
+    total: 0
+  })
+
+  useEffect(() => {
+
+    const fetchBillBoardReport = async () => {
+      const res = await getBoardReport()
+      if (res.status === 200) {
+        setBillboardReport(res.data)
       }
-      fetchBillBoardReport()
-    },[])
+    }
+    fetchBillBoardReport()
+  }, [])
 
   useEffect(() => {
     const fetchBillboards = async () => {
@@ -27,21 +33,46 @@ export default function ManageBillBoardPage() {
       if (res.status === 200) {
         setLoadingTable(false)
         setBillboards(res.data)
-      }else{
+        setPaging({
+          page: res.data.page,
+          size: res.data.page_size,
+          total_pages: res.data.total_pages,
+          total: res.data.total
+        })
+      } else {
         setLoadingTable(false)
       }
     }
     fetchBillboards()
   }, [])
 
-  const searchBillBoard = async(search) => {
-          const res = await getBillBoards({
-              search: search
-          })
-          if (res.status === 200) {
-            setBillboards(res.data)
-          }
-      }
+  const searchBillBoard = async (search) => {
+    const res = await getBillBoards({
+      search: search
+    })
+    if (res.status === 200) {
+      setBillboards(res.data)
+    }
+  }
+  const fetchNextCurrentPage = async (page) => {
+    setBillboards(null)
+    setLoadingTable(true)
+    const res = await getBillBoards({
+      page: page
+    })
+    if (res.status === 200) {
+      setLoadingTable(false)
+      setBillboards(res.data)
+      setPaging((previous) => {
+        return {
+          ...previous,
+          page: res.data.page
+        }
+      })
+    } else {
+      setLoadingTable(false)
+    }
+  }
   return (
     <>
       <div className='d-flex justify-content-between'>
@@ -89,48 +120,75 @@ export default function ManageBillBoardPage() {
           </div>
         </div>
         <div className='card-body'>
-          {loadingTable ? <ShimmerTable    row={5} col={5} loadingAnimation="pulse" /> : <>
-          <div className='table-responsive'>
-            <table className="table  table-hover">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Code</th>
-                  <th>Coordinates</th>
-                  <th>width</th>
-                  <th>height</th>
-                  <th>Measurement</th>
-                  <th>material</th>
-                  <th>price</th>
-                  <th>Status</th>
-                  <th>location</th>
-                  <th className='text-end'>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {billboards && billboards.data.map((billboard, index) => (
-                  <tr key={billboard.id}>
-                    <td>{index + 1}</td>
-                    <td>{billboard.boardCode}</td>
-                    <td>{billboard.latitude}, {billboard.longitude}</td>
-                    <td>{billboard.width}</td>
-                    <td>{billboard.height}</td>
-                    <td>{billboard.unit}</td>
-                    <td>{billboard.type}</td>
-                    <td>{billboard.price}</td>
-                    <td>{billboard.active ? <span className='badge text-bg-success'>active</span> : <span className='badge text-bg-danger'>inactive</span>}</td>
-                    <td>{billboard.location}</td>
-                    <td className='text-end'>
-                      <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-primary me-1 mb-1 d-inline  p-1 global-size' ><Edit className='me-3' size={12} /> Edit</Link>
-                      <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-secondary me-1 mb-1 d-inline  p-1 global-size' ><ListCollapse className='me-3' size={12} /> manage</Link>
-                      <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-danger me-1 mb-2 d-inline  p-1 global-size' ><CopyX className='me-3' size={12} /> Delete</Link>
-                    </td>
+          {loadingTable ? <ShimmerTable row={5} col={5} loadingAnimation="pulse" /> : <>
+            <div className='table-responsive'>
+              <table className="table  table-hover">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Code</th>
+                    <th>Coordinates</th>
+                    <th>width</th>
+                    <th>height</th>
+                    <th>Measurement</th>
+                    <th>material</th>
+                    <th>price</th>
+                    <th>Status</th>
+                    <th>location</th>
+                    <th className='text-end'>Action</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody>
+                  {billboards && billboards.data.map((billboard, index) => (
+                    <tr key={billboard.id}>
+                      <td>{index + 1}</td>
+                      <td>{billboard.boardCode}</td>
+                      <td>{billboard.latitude}, {billboard.longitude}</td>
+                      <td>{billboard.width}</td>
+                      <td>{billboard.height}</td>
+                      <td>{billboard.unit}</td>
+                      <td>{billboard.type}</td>
+                      <td>{billboard.price}</td>
+                      <td>{billboard.active ? <span className='badge text-bg-success'>active</span> : <span className='badge text-bg-danger'>inactive</span>}</td>
+                      <td>{billboard.location}</td>
+                      <td className='text-end'>
+                        <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-primary me-1 mb-1 d-inline  p-1 global-size' ><Edit className='me-3' size={12} /> Edit</Link>
+                        <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-secondary me-1 mb-1 d-inline  p-1 global-size' ><ListCollapse className='me-3' size={12} /> manage</Link>
+                        <Link to={`/billboard/${billboard.id}`} className='btn btn-subtle-danger me-1 mb-2 d-inline  p-1 global-size' ><CopyX className='me-3' size={12} /> Delete</Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </>}
+          <nav aria-label="Page navigation example">
+            <ul className="pagination mb-0">
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  <span className="fas fa-chevron-left"> </span>
+                </a>
+              </li>
+              {paging && Array.from({ length: paging.total_pages }, (_, index) => {
+                const pageNumber = index + 1;
+                return (
+                  <li
+                    key={pageNumber}
+                    className={`page-item ${paging.page === pageNumber ? "active" : ""}`}
+                    onClick={() => fetchNextCurrentPage(pageNumber)}
+                  >
+                    <a className="page-link" href="#">
+                      {pageNumber}
+                    </a>
+                  </li>
+                );
+              })}
+            
+              <li class="page-item">
+                <a class="page-link" href="#"> <span class="fas fa-chevron-right"></span></a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </>
